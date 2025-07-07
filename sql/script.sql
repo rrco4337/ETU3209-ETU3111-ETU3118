93fca21e300f84ded27526885614f628781f4a34
Stanley <mandresystanley@gmail.com>
2025-07-07 11:40:52 +0300

    sql

--- sql/script.sql
+++ sql/script.sql
@@ -2,10 +2,73 @@ CREATE DATABASE tp_flight CHARACTER SET utf8mb4;
 
 USE tp_flight;
 
-CREATE TABLE etudiant (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    email VARCHAR(100),
-    age INT
+-- CREATE TABLE etudiant (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     nom VARCHAR(100),
+--     prenom VARCHAR(100),
+--     email VARCHAR(100),
+--     age INT
+-- );
+
+-- ============================
+-- SCHEMA : Gestion de prêts
+-- ============================
+
+-- TABLE : EF_Fond_Financier
+CREATE TABLE EF_Fond_Financier (
+    idFond SERIAL PRIMARY KEY,
+    solde_initiale DECIMAL(12,2) NOT NULL DEFAULT 0,,
+    solde_final DECIMAL(12,2) DEFAULT 0,,
+    solde_en_cours DECIMAL(12,2) DEFAULT 0,
+    date_creation DATE DEFAULT CURRENT_DATE
 );
+
+-- TABLE : EF_TypePret
+CREATE TABLE EF_TypePret (
+    idType SERIAL PRIMARY KEY,
+    libelle VARCHAR(100) NOT NULL,
+    montant DECIMAL(12,2) NOT NULL,
+
+    taux DECIMAL(5,2) NOT NULL, -- taux d’intérêt en pourcentage
+    duree_mois_max INT NOT NULL
+);
+
+-- TABLE : EF_Client
+CREATE TABLE EF_Client (
+    idClient SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    mail VARCHAR(100),
+    datenaissance DATE,
+    profession VARCHAR(100),
+    salaire_actuel DECIMAL DEFAULT 0,
+    adresse VARCHAR(255),
+    photo_client VARCHAR(255),
+    telephone VARCHAR(20)
+);
+
+CREATE TABLE Prevision_Client (
+    idClient INT PRIMARY KEY,
+    montant NUMERIC(10, 2) DEFAULT 0
+);
+
+-- TABLE : EF_Pret_Client
+CREATE TABLE EF_Pret_Client (
+    idPret SERIAL PRIMARY KEY,
+    idTypePret INT NOT NULL,
+    idClient INT NOT NULL,
+     status INT DEFAULT 0 CHECK (status IN (0, 1, 2, 3, 4)),
+    date_debut_pret DATE DEFAULT CURRENT_DATE,
+    montant_paye  DECIMAL DEFAULT 0,
+    montant_restant DECIMAL DEFAULT 0,
+    date_maj DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (idTypePret) REFERENCES EF_TypePret(idType),
+    FOREIGN KEY (idClient) REFERENCES EF_Client(idClient)
+);
+
+CREATE TABLE EF_Admin (
+    idAdmin INT PRIMARY KEY AUTO_INCREMENT,
+    mail VARCHAR(100) NOT NULL,
+    motdepasse VARCHAR(255) NOT NULL
+);
+
+
