8ae50f7a6f6f014e1db233fd06c76bffb113b2ba
Fanitsy12 <andrainafanitsy@gmail.com>
2025-07-07 11:47:53 +0300

    feature/client

--- ws/db.php
+++ ws/db.php
@@ -3,7 +3,7 @@ function getDB() {
     $host = 'localhost';
     $dbname = 'tp_flight';
     $username = 'root';
-    $password = 'root';
+    $password = '';
 
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
